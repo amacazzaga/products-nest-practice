48096ceee817426b58d8f68a130468dad2f1497f
unknown <ag.macazz@gmail.com>
2023-12-26 16:32:42 -0300

    some examples on controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
+@Controller("products")
+export class AppController {// clase
   constructor(private readonly appService: AppService) {}
 
+  @Get("categories") //decorador
+  findAll(): string[] {
+    return ["telefonos","tablets"]; //a modo de ejemplo
+  }
+  @Get(":id")
+  findOne(@Param() params: any): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} product by id`;
+  }
+}
+// ejemplo de como usando host podemos entender que la request es de admin y devolverle la pagina al adm de nuestros products
+@Controller({ host: 'admin.example.com' })
+export class AdminController {
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  index(): string {
+    return 'Admin page';
   }
 }

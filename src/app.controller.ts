96d73791d79f4b32ad6f857bd53d21f1404a79d1
unknown <ag.macazz@gmail.com>
2023-12-26 17:10:29 -0300

    post example on product controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,28 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+
+/*nuestro DTO es un objeto que declara el tipo del producto es usado solo en el metodo de POST*/
+export class ProductDto {
+  name: string;
+  age: number;
+  breed: string;
+}
 
 @Controller("products")
 export class AppController {// clase
   constructor(private readonly appService: AppService) { }
-
+  @Post() //decorador (metodo http)
+  create(@Body() createProductDto: ProductDto) {
+    return 'This action adds a new product';
+  }
   @Get("categories") //decorador
   findAll(): string[] {
     return ["telefonos", "tablets"]; //a modo de ejemplo
   }
-  @Get(":id")
+  @Get(":id") //decorador
   findOne(@Param() params: any): string {
     console.log(params.id);
-    return `This action returns a #${params.id} product by id`;
+    return `This action returns a ${params.id} product by id`;
   }
 }
 // ejemplo de como usando host podemos entender que la request es de admin y devolverle la pagina al adm de nuestros products
@@ -23,3 +33,5 @@ export class AdminController {
     return 'Admin page';
   }
 }
+
+

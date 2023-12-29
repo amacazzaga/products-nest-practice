01c571da05ed03c64659807580bf10fa1011e619
unknown <ag.macazz@gmail.com>
2023-12-29 12:42:26 -0300

    global filter example on main

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //app.useGlobalFilters(new HttpExceptionFilter()); asi se veria app si usaramos un global filter!
   await app.listen(3000);
 }
 bootstrap();

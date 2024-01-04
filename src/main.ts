403351f202ce33b9dd334b549ad93babb227f410
agustom <agustin.macazzaga@codiced.ev>
2024-01-04 12:16:47 -0300

    UseInterceptor pass in a global level

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RolesGuard } from './authorization/role.guard';
+import { LoggingInterceptor } from './interceptor/loggin.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //app.useGlobalFilters(new HttpExceptionFilter()); asi se veria app si usaramos un global filter!
   //app.useGlobalPipes(new ValidationPipe()); asi podriamos pasar un pipe a nivel global!
+  app.useGlobalInterceptors( new LoggingInterceptor()) //pasamos el interceptor de login global pero con new en este caso,proporcionando una nueva instancia
   app.useGlobalGuards(new RolesGuard()); // asi podemos pasar un guard a nivel global, solo que este ejemplo no proteje ningun endpoint: ver role.guard.ts
   await app.listen(3000);
 }

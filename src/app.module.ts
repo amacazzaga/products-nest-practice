da5298baa258751b933d0e00eab74b930f9feb74
agustom <agustin.macazzaga@codiced.ev>
2024-01-04 12:37:09 -0300

    Loggin interceptor as provider on @Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Global, Module, NestModule, MiddlewareConsumer, RequestMethod } from '@
 import { AppController } from './app.controller';
 import { ProductService } from './app.service';
 import { myMiddleware, myMiddlewareFunction } from './middleware/middleware';
+import { LoggingInterceptor } from './interceptor/loggin.interceptor';
 // Este es nuestro app module, si tuvieramos shared modules deberian estar todos volcados aqui, por ej :  imports: [ProductsModule] import :[UsersModule],
 
 @Global() //podemos hacer este modulo global con este decorador para no tener que exportarlo muchisimas veces
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [ProductService],
+  providers: [ProductService,LoggingInterceptor] // pasamos el loggin interceptor aparte del servicio aqui en el modulo de productos como ejemplo
 })
 
 // asi se pasa el middleware en modules, aqui esta importado de la carpeta donde se creo y se ejecutará en todas las rutas de "products" que sean un GET

23ad665ea511f9b7e83492e3726cc9b6961f3909
amacazzagacodice <agustin.macazzaga@codice.dev>
2024-01-17 15:43:00 -0300

    comment on main

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { LoggingInterceptor } from './interceptor/loggin.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //app.useGlobalFilters(new HttpExceptionFilter()); asi se veria app si usaramos un global filter!
-  //app.useGlobalPipes(new ValidationPipe()); asi podriamos pasar un pipe a nivel global!
+  //app.useGlobalPipes(new ValidationPipe()); asi podriamos pasar un pipe a nivel global!Cuando aplicas el ValidationPipe de manera global 
+  //utilizando app.useGlobalPipes(new ValidationPipe()), estás diciendo a NestJS que todas las solicitudes que lleguen 
+  //a tu aplicación deben ser validadas según las reglas que hayas definido en los DTOs.
   app.useGlobalInterceptors( new LoggingInterceptor()) //pasamos el interceptor de login global pero con new en este caso,proporcionando una nueva instancia
   app.useGlobalGuards(new RolesGuard()); // asi podemos pasar un guard a nivel global, solo que este ejemplo no proteje ningun endpoint: ver role.guard.ts
   await app.listen(3000);
